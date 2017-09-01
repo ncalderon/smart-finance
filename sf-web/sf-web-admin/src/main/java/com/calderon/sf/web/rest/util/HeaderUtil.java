014f8ea3466fbac21a9700a2ed942578e9d348ed
ncalderon <nathaniel.calderon@gmail.com>
2017-09-01 15:11:11 -0400

    Add sf-web-admin module

---
+++ sf-web/sf-web-admin/src/main/java/com/calderon/sf/web/rest/util/HeaderUtil.java
@@ -0,0 +1,44 @@
+package com.calderon.sf.web.rest.util;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpHeaders;
+/**
+ * Utility class for HTTP headers creation.
+ */
+public final class HeaderUtil {
+
+    private static final Logger log = LoggerFactory.getLogger(HeaderUtil.class);
+
+    private static final String APPLICATION_NAME = "sfWebAdminApp";
+
+    private HeaderUtil() {
+    }
+
+    public static HttpHeaders createAlert(String message, String param) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-sfWebAdminApp-alert", message);
+        headers.add("X-sfWebAdminApp-params", param);
+        return headers;
+    }
+
+    public static HttpHeaders createEntityCreationAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".created", param);
+    }
+
+    public static HttpHeaders createEntityUpdateAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".updated", param);
+    }
+
+    public static HttpHeaders createEntityDeletionAlert(String entityName, String param) {
+        return createAlert(APPLICATION_NAME + "." + entityName + ".deleted", param);
+    }
+
+    public static HttpHeaders createFailureAlert(String entityName, String errorKey, String defaultMessage) {
+        log.error("Entity processing failed, {}", defaultMessage);
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-sfWebAdminApp-error", "error." + errorKey);
+        headers.add("X-sfWebAdminApp-params", entityName);
+        return headers;
+    }
+}
